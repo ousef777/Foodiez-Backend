ac402afcb9d784e19c506750f318d98eaa1d02f8
eng-batol <batol.alashwak@gmail.com>
2024-11-27 08:35:07 +0300

    installing multer / env / creat new folder config / create file to uplode image

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 const connectDB = async () => {
+  
   const conn = await mongoose.connect(process.env.DB_URL);
   console.log(`mongo connected: ${conn.connection.host}`);
 };
